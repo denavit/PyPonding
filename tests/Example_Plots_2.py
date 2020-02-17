7451580699e73c4aad15c4381d3a12e6f535013c
denavit <denavit@gmail.com>
2020-02-16 21:01:57 -0500

    Formatting the plots

--- tests/Example_Plots_2.py
+++ tests/Example_Plots_2.py
@@ -13,6 +13,10 @@ in_per_ft = inch/ft
 ksi = kip/inch**2
 psf = lb/ft**2
 pcf = psf/ft
+mm  = 1/25.4*inch
+m   = 1000*mm
+kN  = 1/4.448222*kip
+kNm = kN*m
 
 # Input parameters
 Fy      = 50.0*ksi
@@ -25,7 +29,8 @@ L2      = 49.57*ft
 L3      = 54.85*ft
 slope   = 0.0*in_per_ft
 qD      = 0.0*psf
- 
+gamma   = 62.4*pcf
+
 # Lookup shape data
 shape_data = wf_shapes[shape_name]
 d  = shape_data['d']*inch
@@ -38,7 +43,7 @@ wf_section = wf(d,tw,bf,tf,Fy,E,Hk)
 
 # Set additional properties
 wf_section.L        = L1
-wf_section.gamma    = 62.4*pcf
+wf_section.gamma    = gamma
 wf_section.TW       = TW
 wf_section.zi       = 0.0*inch
 wf_section.zj       = L1*slope
@@ -47,7 +52,7 @@ wf_section.wD       = qD*TW # Dead load per length
 # Set OpenSees analysis options
 wf_section.material_type = 'Elastic'
 wf_section.num_steps = 2000
-
+wf_section.percent_drop = 1.0
 
 # Run OpenSees analyses 
 # L1
@@ -101,49 +106,69 @@ slope2 = (pi*C2**0.25)/(TW*L2*(tan(0.5*pi*C2**0.25)+tanh(0.5*pi*C2**0.25)))
 slope3 = (pi*C3**0.25)/(TW*L3*(tan(0.5*pi*C3**0.25)+tanh(0.5*pi*C3**0.25)))
 
 # Plot Results
-plt.figure(1)  
-line1, = plt.plot(data_volume_L1/(TW*L1), data_height_L1, '-')
-line2, = plt.plot(data_volume_L2/(TW*L2), data_height_L2, '-')
-line3, = plt.plot(data_volume_L3/(TW*L3), data_height_L3, '-')
+plt.rc('text',usetex=True)
+plt.rc('font',family='serif')
+plt.rc('axes',labelsize=8)
+plt.rc('axes',titlesize=8)
+plt.rc('legend',fontsize=8)
+plt.rc('xtick',labelsize=8)
+plt.rc('ytick',labelsize=8)
 
-line4, = plt.plot(np.array([0,10]), np.array([0,10*TW*L1*slope1]), 'o')
-line5, = plt.plot(np.array([0,10]), np.array([0,10*TW*L2*slope2]), 'o')
-line6, = plt.plot(np.array([0,10]), np.array([0,10*TW*L3*slope3]), 'o')
-
+fig = plt.figure(figsize=(3.25,2.50))
+ax = fig.add_axes([0.18,0.18,0.78,0.78])
+line1, = plt.plot(data_volume_L1/(TW*L1)/mm, data_height_L1/mm, 'k-')
+line2, = plt.plot(data_volume_L2/(TW*L2)/mm, data_height_L2/mm, 'k--')
+line3, = plt.plot(data_volume_L3/(TW*L3)/mm, data_height_L3/mm, 'k-.')
+x_closedform = np.linspace(0.0, 250.0, num=10)
+line4, = plt.plot(x_closedform,x_closedform*TW*L1*slope1,'ko', markersize=4)
+line5, = plt.plot(x_closedform,x_closedform*TW*L2*slope2,'ko', markersize=4)
+#line6, = plt.plot(x_closedform,x_closedform*TW*L3*slope3,'ko', markersize=4)
 print(C1)
 print(C2)
 print(C3)
+plt.legend((line4,line1,line2,line3), ('Closed-form Solution','C = 0.424','C = 1.0','C = 1.5'),frameon=False)
+plt.xlabel('Normalized Water Volume (V/SL, mm)')
+plt.ylabel('Water Height (mm)')
+plt.xlim( 0,250)
+plt.ylim(-250,250)
+plt.savefig('Example_Plot_1.png',dpi=300)
+plt.savefig('Example_Plot_1.pdf')
 
-plt.legend((line1, line2, line3), ('C = 0.424', 'C = 1.0', 'C = 1.5'))
-plt.xlabel('Normalized Water Volume (V/SL)')
-plt.ylabel('Water Height')
-plt.xlim( 0,10)
-plt.ylim(-7,7)
+fig = plt.figure(figsize=(3.25,2.50))
+ax = fig.add_axes([0.15,0.18,0.80,0.78])
+line1, = plt.plot(data_volume_L1s/(TW*L1)/mm, data_height_L1s/mm, 'k-')
+line2, = plt.plot(data_volume_L2s/(TW*L2)/mm, data_height_L2s/mm, 'k--')
+line3, = plt.plot(data_volume_L3s/(TW*L3)/mm, data_height_L3s/mm, 'k-.')
+plt.legend((line1, line2, line3), ('C = 0.424', 'C = 1.0', 'C = 1.5'),frameon=False)
+plt.xlabel('Normalized Water Volume (V/SL, mm)')
+plt.ylabel('Water Height (mm)')
+plt.xlim(0,250)
+plt.ylim(0,300)
+plt.savefig('Example_Plot_2.png',dpi=300)
+plt.savefig('Example_Plot_2.pdf')
 
+fig = plt.figure(figsize=(3.25,5.00))
+ax = fig.add_axes([0.15,0.59,0.80,0.39])  
+line1, = plt.plot(   data_volume_L1s/(TW*L1)/mm,    data_height_L1s/mm, 'k-')
+line2, = plt.plot(  data_volume_L1si/(TW*L1)/mm,   data_height_L1si/mm, 'k--')
+line3, = plt.plot(data_volume_L1sirs/(TW*L1)/mm, data_height_L1sirs/mm, 'r-.')
+plt.legend((line1, line2, line3), ('Elastic', 'Inelastic (no residual stress)', 'Inelastic'),frameon=False)
+plt.xlabel('Normalized Water Volume (V/SL, mm)')
+plt.ylabel('Water Height (mm)')
+plt.xlim(0,350)
+plt.ylim(0,350)
 
+ax = fig.add_axes([0.15,0.09,0.80,0.39])  
+line1, = plt.plot(   data_volume_L1s/(TW*L1)/mm,    data_height_L1s/mm, 'k-')
+line2, = plt.plot(  data_volume_L1si/(TW*L1)/mm,   data_height_L1si/mm, 'k--')
+line3, = plt.plot(data_volume_L1sirs/(TW*L1)/mm, data_height_L1sirs/mm, 'r-.')
+plt.legend((line1, line2, line3), ('Elastic', 'Inelastic (no residual stress)', 'Inelastic'),frameon=False)
+plt.xlabel('Normalized Water Volume (V/SL, mm)')
+plt.ylabel('Water Height (mm)')
+plt.xlim(210,290)
+plt.ylim(250,320)
 
-plt.figure(2)  
-line1, = plt.plot(data_volume_L1s/(TW*L1), data_height_L1s, '-')
-line2, = plt.plot(data_volume_L2s/(TW*L2), data_height_L2s, '-')
-line3, = plt.plot(data_volume_L3s/(TW*L3), data_height_L3s, '-')
-
-plt.legend((line1, line2, line3), ('C = 0.424', 'C = 1.0', 'C = 1.5'))
-plt.xlabel('Normalized Water Volume (V/SL)')
-plt.ylabel('Water Height')
-plt.xlim(0,10)
-plt.ylim(0,12)
-
-
-
-plt.figure(3)  
-line1, = plt.plot(   data_volume_L1s/(TW*L1),    data_height_L1s, '-')
-line2, = plt.plot(  data_volume_L1si/(TW*L1),   data_height_L1si, '-')
-line3, = plt.plot(data_volume_L1sirs/(TW*L1), data_height_L1sirs, '-')
-
-plt.legend((line1, line2, line3), ('Elastic', 'Inelastic', 'Inelastic with RS'))
-plt.xlabel('Normalized Water Volume (V/SL)')
-plt.ylabel('Water Height')
-plt.xlim(0,14)
-plt.ylim(0,14)
+plt.savefig('Example_Plot_3.png',dpi=300)
+plt.savefig('Example_Plot_3.pdf')
 
 plt.show()
