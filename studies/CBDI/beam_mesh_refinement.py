535fba7e1109a980f04540242c74dd4bca791ecd
denavit <denavit@gmail.com>
2020-10-03 22:36:13 -0400

    Create beam_mesh_refinement.py

---
+++ studies/CBDI/beam_mesh_refinement.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from structures import ExampleBeam
+from PyPonding.structures import wf_shapes
+from math import pi
+
+# Define beam
+shape_data = wf_shapes['W14X22']
+d  = shape_data['d']
+tw = shape_data['tw']
+bf = shape_data['bf']
+tf = shape_data['tf']
+Fy = 50
+E  = 29000
+L  = 40*12
+S  = 10*12
+qD = 0./1000/12**2    # Uniform dead load (forcer per unit area, downward positive)
+zw = 6
+
+beam = ExampleBeam(d,tw,bf,tf,Fy,E,L,S,qD)
+beam.material_type = 'ElasticSection'
+
+I = beam.Iz
+gamma = beam.gamma
+
+C = (gamma*S*L**4)/(pi**4*E*I)
+amp = 1/(1-C)
+
+# Initilize results
+num_divisions = np.array([1,2,4,8,12,16,20,24,28,32])
+water_volume_1 = np.zeros(np.size(num_divisions))
+water_volume_2 = np.zeros(np.size(num_divisions))
+
+# Run analyses
+for i in range(len(num_divisions)):
+    # num_elements = num_divisions
+    beam.num_elements = int(num_divisions[i])
+    beam.num_divisions_per_element = 1
+    beam.element_type = 'dispBeamColumn'
+    beam.nIP = 3
+    results = beam.RunAnalysis('IterativeLevel' ,target_zw=zw)
+    water_volume_1[i] = results.water_volume
+    
+    # num_elements = 1
+    beam.num_elements = 1
+    beam.num_divisions_per_element = int(num_divisions[i])
+    beam.element_type = 'forceBeamColumn'
+    beam.nIP = 8
+    results = beam.RunAnalysis('IterativeLevel' ,target_zw=zw)
+    water_volume_2[i] = results.water_volume
+
+print(amp)
+    
+# Plot Results
+plt.rc('text',usetex=True)
+plt.rc('font',family='serif')
+plt.rc('axes',labelsize=8)
+plt.rc('axes',titlesize=8)
+plt.rc('legend',fontsize=8)
+plt.rc('xtick',labelsize=8)
+plt.rc('ytick',labelsize=8)
+
+fig = plt.figure(figsize=(3.25,2.50))
+ax = fig.add_axes([0.18,0.18,0.78,0.78])
+line0, = plt.plot([0,35], [amp,amp],'k-')
+line1, = plt.plot(num_divisions, water_volume_1/(S*L*zw),'ko--')
+line2, = plt.plot(num_divisions, water_volume_2/(S*L*zw),'ro--')
+plt.legend((line0,line1,line2), ('Analytical','$n_{ele} = n_{div}$','$n_{ele} = 1$'),frameon=False)
+plt.xlabel('Number of Divisions')
+plt.ylabel('Normalized Water Volume ($V/SLz_w$)')
+plt.xlim(0,35)
+plt.ylim(1.0,1.65)
+plt.savefig('Beam_Mesh_Refinement_1.png',dpi=300)
+plt.savefig('Beam_Mesh_Refinement_1.pdf')
+
+plt.show()
