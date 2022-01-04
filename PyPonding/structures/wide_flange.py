e69c5183a75b3c16743ace07e3a29e0b4b8a4f79
Minjie Zhu <zhum@oregonstate.edu>
2022-01-03 16:57:43 -0800

    match units

--- PyPonding/structures/wide_flange.py
+++ PyPonding/structures/wide_flange.py
@@ -1,134 +1,164 @@
 import numpy as np
-from math import pi,ceil
+from math import pi, ceil
 from PyPonding import PondingLoadCell2d_OPS
 from PyPonding.structures import steel_beam
 
-import openseespy.opensees as ops
+try:
+    import opensees as ops
+except ImportError:
+    try:
+        import openseespy.opensees as ops
+    except ImportError:
+        import warnings
+        warnings.warn(
+            'OpenSeesPy not found. OpenSees functionality is not available.')
+
 
 class wf:
-    geomTransfType = 'Linear'
+    geomTransfType = 'Corotational'
     extra_results = False
 
-    def __init__(self,d,tw,bf,tf,Fy,E,Hk):
-        self.d  = d             # Depth
+    def __init__(self, d, tw, bf, tf, Fy, E, Hk):
+        self.d = d             # Depth
         self.tw = tw            # Thickness of the web
         self.bf = bf            # Width of the flange
         self.tf = tf            # Thickness of the flnage
         self.Fy = Fy            # Yield stree
-        self.E  = E             # Elastic modulus
-        self.Hk = Hk            # Kinematic hardening modulus 
-        self.frc = 0.0          # Maximum compressive residual stress (Lehigh pattern)
+        self.E = E             # Elastic modulus
+        self.Hk = Hk            # Kinematic hardening modulus
+        # Maximum compressive residual stress (Lehigh pattern)
+        self.frc = 0.0
         self.num_regions = 10   # Number of regions for the discretization of residual stress
-        
-        self.L  = float('nan')  # Beam length (span)
+
+        self.L = float('nan')  # Beam length (span)
         self.TW = float('nan')  # Tributary width
         self.gamma = 0.0
-        
+
         self.zi = 0.0
         self.zj = 0.0
-        self.c  = 0.0           # Camber
-        
+        self.c = 0.0           # Camber
+
         self.wD = 0.0           # Dead load (force per unit length)
-                
+
         self.material_type = 'Hardening'
         self.num_elements = 20
         self.num_fiber = 20
         self.num_steps = 100
-        
+
         self.nsteps_vol = 30
         self.max_volume = float('nan')
         self.vol_tol = float('nan')
         self.percent_drop = 0.05
-        
+
     def dw(self):
         dw = self.d-2*self.tf
         return dw
-    
+
     def A(self):
         A = 2*self.bf*self.tf + (self.d-2*self.tf)*self.tw
         return A
-    
+
     def Iz(self):
-        Iz = (1.0/12)*self.bf*self.d**3 - (1.0/12)*(self.bf-self.tw)*self.dw()**3
+        Iz = (1.0/12)*self.bf*self.d**3 - (1.0/12) * \
+              (self.bf-self.tw)*self.dw()**3
         return Iz
-     
+
     def Sz(self):
         Sz = self.Iz()/(self.d/2)
         return Sz
-     
+
     def Zz(self):
-        Zz = 2*((self.tf*self.bf)*(self.d/2-self.tf/2) + (self.dw()/2*self.tw)*(self.dw()/4));
+        Zz = 2*((self.tf*self.bf)*(self.d/2-self.tf/2) +
+                (self.dw()/2*self.tw)*(self.dw()/4));
         return Zz
-       
+
     def Mp(self):
         return self.Fy*self.Zz()
-       
+
     def C(self):
         return (self.gamma*self.TW*self.L**4)/(pi**4*self.E*self.Iz())
-        
-    def define_fiber_section(self,secTag,matTag):
+
+    def define_fiber_section(self, secTag, matTag):
         Nfw = ceil(self.dw()*(self.num_fiber/self.d))
         Nff = ceil(self.tf*(self.num_fiber/self.d))
-        
+
         if self.frc == 0 or self.material_type == 'Elastic':
             if self.material_type == 'Elastic':
                 ops.uniaxialMaterial('Elastic', matTag, self.E)
             elif self.material_type == 'ElasticPP':
-                ops.uniaxialMaterial('ElasticPP', matTag, self.E, self.Fy/self.E)
+                ops.uniaxialMaterial('ElasticPP', matTag,
+                                     self.E, self.Fy/self.E)
             elif self.material_type == 'Steel01':
                 b = self.Hk/(self.E+self.Hk)
                 ops.uniaxialMaterial('Steel01', matTag, self.Fy, self.E, b)
             elif self.material_type == 'Hardening':
-                ops.uniaxialMaterial('Hardening', matTag, self.E, self.Fy, 0.0, self.Hk)
+                ops.uniaxialMaterial('Hardening', matTag,
+                                     self.E, self.Fy, 0.0, self.Hk)
             else:
-                raise Exception('Input Error - unknown material type (%s)' % self.material_type)
-            
-            ops.section('WFSection2d',secTag,matTag,self.d,self.tw,self.bf,self.tf,Nfw,Nff)
-        else: 
-            ops.section('Fiber', secTag)    
-            
-            frt = -self.frc*(self.bf*self.tf)/(self.bf*self.tf+self.tw*self.dw())
-            
+                raise Exception(
+                    'Input Error - unknown material type (%s)' % self.material_type)
+
+            ops.section('WFSection2d', secTag, matTag, self.d,
+                        self.tw, self.bf, self.tf, Nfw, Nff)
+        else:
+            ops.section('Fiber', secTag)
+
+            frt = -self.frc*(self.bf*self.tf) / \
+                             (self.bf*self.tf+self.tw*self.dw())
+
             # Define web fibers
             if self.material_type == 'ElasticPP':
-                ops.uniaxialMaterial('ElasticPP', matTag, self.E, self.Fy/self.E, -self.Fy/self.E, frt/self.E)
+                ops.uniaxialMaterial(
+                    'ElasticPP', matTag, self.E, self.Fy/self.E, -self.Fy/self.E, frt/self.E)
             elif self.material_type == 'Steel01':
                 b = self.Hk/(self.E+self.Hk)
                 ops.uniaxialMaterial('Steel01', matTag+1, self.Fy, self.E, b)
-                ops.uniaxialMaterial('InitStressMaterial', matTag , matTag+1, frt)
+                ops.uniaxialMaterial('InitStressMaterial',
+                                     matTag, matTag+1, frt)
             elif self.material_type == 'Hardening':
-                ops.uniaxialMaterial('Hardening', matTag+1, self.E, self.Fy, 0.0, self.Hk)
-                ops.uniaxialMaterial('InitStressMaterial', matTag , matTag+1, frt)
+                ops.uniaxialMaterial('Hardening', matTag+1,
+                                     self.E, self.Fy, 0.0, self.Hk)
+                ops.uniaxialMaterial('InitStressMaterial',
+                                     matTag, matTag+1, frt)
             else:
-                raise Exception('Input Error - unknown material type (%s)' % self.material_type)
-            ops.patch('rect', matTag, Nfw, 1, -self.dw()/2, -self.tw/2, self.dw()/2, self.tw/2)
-                      
+                raise Exception(
+                    'Input Error - unknown material type (%s)' % self.material_type)
+            ops.patch('rect', matTag, Nfw, 1, -self.dw() /
+                      2, -self.tw/2, self.dw()/2, self.tw/2)
+
             # Define flange fibers
             region_width = self.bf/self.num_regions
             for i in range(self.num_regions):
                 fri = self.frc + ((i+0.5)/self.num_regions)*(frt-self.frc)
-            
+
                 matTagi = matTag+2*(i+1)
                 if self.material_type == 'ElasticPP':
-                    ops.uniaxialMaterial('ElasticPP', matTagi, self.E, self.Fy/self.E, -self.Fy/self.E, fri/self.E)
+                    ops.uniaxialMaterial(
+                        'ElasticPP', matTagi, self.E, self.Fy/self.E, -self.Fy/self.E, fri/self.E)
                 elif self.material_type == 'Steel01':
                     b = self.Hk/(self.E+self.Hk)
-                    ops.uniaxialMaterial('Steel01', matTagi+1, self.Fy, self.E, b)
-                    ops.uniaxialMaterial('InitStressMaterial', matTagi , matTagi+1, fri)
+                    ops.uniaxialMaterial(
+                        'Steel01', matTagi+1, self.Fy, self.E, b)
+                    ops.uniaxialMaterial(
+                        'InitStressMaterial', matTagi, matTagi+1, fri)
                 elif self.material_type == 'Hardening':
-                    ops.uniaxialMaterial('Hardening', matTagi+1, self.E, self.Fy, 0.0, self.Hk)
-                    ops.uniaxialMaterial('InitStressMaterial', matTagi , matTagi+1, fri)
+                    ops.uniaxialMaterial(
+                        'Hardening', matTagi+1, self.E, self.Fy, 0.0, self.Hk)
+                    ops.uniaxialMaterial(
+                        'InitStressMaterial', matTagi, matTagi+1, fri)
                 else:
-                    raise Exception('Input Error - unknown material type (%s)' % self.material_type)
-            
-                ops.patch('rect', matTagi, Nff, 1, self.dw()/2, -region_width/2, self.d/2, region_width/2)
-                ops.patch('rect', matTagi, Nff, 1, -self.d/2, -region_width/2, -self.dw()/2, region_width/2)
+                    raise Exception(
+                        'Input Error - unknown material type (%s)' % self.material_type)
+
+                ops.patch('rect', matTagi, Nff, 1, self.dw()/2, -
+                          region_width/2, self.d/2, region_width/2)
+                ops.patch('rect', matTagi, Nff, 1, -self.d/2, -
+                          region_width/2, -self.dw()/2, region_width/2)
         return
-        
-        
+
     def perform_OpenSees_analysis(self):
         mid_node = int(self.num_elements/2)
-        
+
         # set modelbuilder
         ops.model('basic', '-ndm', 2, '-ndf', 3)
 
@@ -140,32 +170,31 @@ class wf:
             if self.c != 0:
                 r = (self.c**2 + (self.L/2)**2)/(2*self.c)
                 iy += sqrt(r**2 - (self.L*x_over_L - self.L/2)**2) + (self.c-r)
-            ops.node(i,ix,iy)
+            ops.node(i, ix, iy)
 
         # set boundary condition
-        ops.fix(0,1,1,0)
-        ops.fix(self.num_elements,0,1,0)
+        ops.fix(0, 1, 1, 0)
+        ops.fix(self.num_elements, 0, 1, 0)
 
         # define coordinate transformation
-        ops.geomTransf(self.geomTransfType,1)
+        ops.geomTransf(self.geomTransfType, 1)
 
         # define cross section
-        self.define_fiber_section(1,1)
+        self.define_fiber_section(1, 1)
         ops.beamIntegration('Lobatto', 1, 1, 3)
 
         # Time series for loads
         ops.timeSeries("Constant", 1)
-        
+
         # define elements
-        for i in range(0,self.num_elements):
-            #ops.element("forceBeamColumn",i,i,i+1,1,1)
-            ops.element("dispBeamColumn",i,i,i+1,1,1)            
-        
+        for i in range(0, self.num_elements):
+            # ops.element("forceBeamColumn",i,i,i+1,1,1)
+            ops.element("dispBeamColumn", i, i, i+1, 1, 1)
+
         # Dead load
-        ops.pattern('Plain',-1,1)
-        for i in range(0,self.num_elements):
-            ops.eleLoad('-ele',i,'-type','beamUniform',-self.wD)
-            
+        ops.pattern('Plain', -1, 1)
+        for i in range(0, self.num_elements):
+            ops.eleLoad('-ele', i, '-type', 'beamUniform', -self.wD)
         # ------------------------------
         # Start of analysis generation
         # ------------------------------
@@ -185,6 +214,8 @@ class wf:
         # create algorithm
         ops.algorithm("Newton")
 
+        ops.test('NormDispIncr', 1e-6, 100, 1)
+
         # create analysis object
         ops.analysis("Static")
 
@@ -195,6 +226,9 @@ class wf:
             iz = ops.nodeCoord(i, 2) + ops.nodeDisp(i, 2)
             if iz < zo:
                 zo = iz
+            print(f"node {i}: {ops.nodeDisp(i, 2)}")
+
+        exit()
         
         # Initilize data        
         data_volume = np.zeros((self.num_steps+1,1))
@@ -322,9 +356,9 @@ class wf:
     def maximum_permitted_zw(self,method):
         if method == 'AISC Appendix 2':
             Cs = (self.gamma*self.TW*self.L**4)/(pi**4*self.E*self.Iz())
-            #Us_limit = Cs/(1-Cs)
-            #fo_limit = 0.8*self.Fy/(Us_limit+1)
-            #Mo_limit = fo_limit*self.S
+            # Us_limit = Cs/(1-Cs)
+            # fo_limit = 0.8*self.Fy/(Us_limit+1)
+            # Mo_limit = fo_limit*self.S
             Mo_limit = 0.8*self.Fy*self.Sz()*(1-Cs)
             
             elastic_beam = self.steel_beam_object()
--- PyPondingPFEM.py
+++ PyPondingPFEM.py
@@ -4,17 +4,23 @@ import os
 import matplotlib.pyplot as plt
 
 # Define units
-m = 1.0
-kg = 1.0
+inch = 1.0
+kip = 1.0  # kip-mass
 sec = 1.0
-Newton = kg*m/sec**2
-Pa = Newton / m**2
-inch = 0.0254 * m
+lb = kip/1000.0  # pound-mass
 ft = 12.0*inch
 in_per_ft = inch/ft
-ksi = 6.895e+6 * Pa
-psf = ksi / 1000.0
+m = 39.3701 * inch  # meter
+lbf = 32.174049 * lb*ft/sec**2  # pound-force
+kipf = 1000*lbf  # kip-force
+ksi = kipf/inch**2
+psf = lbf/ft**2
 pcf = psf/ft
+
+kg = 2.2046 * lb
+Newton = kg*m/sec**2
+Pa = Newton/m**2
+MPa = 1e6*Pa
 
 # structure parameters
 Fy = 50.0*ksi
@@ -31,7 +37,7 @@ wD = qD*TW
 shape = {'W': 22, 'A': 6.49, 'd': 13.7, 'ddet': 13.75, 'bf': 5, 'bfdet': 5, 'tw': 0.23, 'twdet': 0.25, 'twdet/2': 0.125, 'tf': 0.335, 'tfdet': 0.3125, 'kdes': 0.735, 'kdet': 1.0625, 'k1': 0.75, 'bf/2tf': 7.46, 'h/tw': 53.3, 'Ix': 199, 'Zx': 33.2, 'Sx': 29,
          'rx': 5.54, 'Iy': 7, 'Zy': 4.39, 'Sy': 2.8, 'ry': 1.04, 'J': 0.208, 'Cw': 314, 'Wno': 16.7, 'Sw1': 7, 'Qf': 5.34, 'Qw': 16.1, 'rts': 1.27, 'ho': 13.4, 'PA': 41.3, 'PB': 46.3, 'PC': 32.4, 'PD': 37.4, 'T': 11.625, 'WGi': 2.75, 'metric_label': 'W360X32.9'}
 
-num_elements = 20
+num_elements = 100
 num_fiber = 20
 eleLen = L / num_elements
 
@@ -54,33 +60,33 @@ kappa = -1.0
 
 bmass = wD * eleLen / abs(b2)
 
+waterVolume = (6*inch)*L
+
 # analysis parameters
 dtmax = 1e-3
 dtmin = 1e-3
 totaltime = 1.0
 filename = "ponding"
+ndf = 3
 
 # model
 ops.wipe()
 
-ops.model('basic', '-ndm', 2, '-ndf', 3)
+ops.model('basic', '-ndm', 2, '-ndf', ndf)
 
 # recorder
-ops.recorder('PVD', filename, 'disp', 'vel', 'pressure', '-dT', 1e-3)
+ops.recorder('BgPVD', filename, 'disp', 'vel', 'pressure', '-dT', 1e-3)
 if not os.path.exists(filename):
   os.makedirs(filename)
-
-# fluid mesh
-ndf = 3
-
-# nx = round(L / eleLen * numx)
-# ny = round(H / h * numy)
 
 # wall mesh
 ops.node(1, 0.0, H)
 ops.node(2, 0.0, 0.0)
 ops.node(3, L, 0.0)
 ops.node(4, L, H)
+
+ops.fix(2, 1, 1, 0)
+ops.fix(3, 1, 1, 0)
 
 sid = 1
 walltag1 = 1
@@ -93,9 +99,11 @@ wallNodes1 = ops.getNodeTags('-mesh', walltag1)
 wallNodes2 = ops.getNodeTags('-mesh', walltag2)
 
 for nd in wallNodes1:
-  ops.fix(nd, 1, 1, 1)
+  if nd != 2 and nd != 3:
+    ops.fix(nd, 1, 1, 1)
 for nd in wallNodes2:
-  ops.fix(nd, 1, 1, 1)
+  if nd != 2 and nd != 3:
+    ops.fix(nd, 1, 1, 1)
 
 # geom transf
 transfTag = 1
@@ -136,9 +144,6 @@ ops.pattern('Plain', patternTag, tsTag)
 for ele in sEles:
   ops.eleLoad('-ele', ele, '-type', 'beamUniform', -wD)
 
-for nd in sNodes:
-  ops.mass(nd, bmass, bmass, 0.0)
-
 # create constraint object
 ops.constraints('Plain')
 
@@ -148,20 +153,75 @@ ops.numberer('Plain')
 # create algorithm object
 ops.algorithm('Newton')
 
+# create convergence test object
+ops.test('NormDispIncr', 1e-6, 100, 1)
+
 # create integrator object
 ops.integrator("LoadControl", 0.0)
 
 # create SOE object
 ops.system("Mumps")
-# system('PFEM')
-# ops.system('PFEM', '-mumps')
 
 # create analysis object
 ops.analysis('Static')
-# ops.analysis('PFEM', dtmax, dtmin, b2)
 
 # run dead load analysis
 ops.analyze(1)
 
 for nd in sNodes:
-  print(f'node {nd} disp = {ops.nodeDisp(nd)}')
+  print(f"node {nd}: {ops.nodeDisp(nd, 2)/inch}")
+
+# wipe analysis
+ops.wipeAnalysis()
+ops.loadConst('-time', 0.0)
+
+# add
+for nd in sNodes:
+  ops.mass(nd, bmass, bmass, 0.0)
+
+# fluid mesh
+Lwater = 20*eleLen
+Hwater = waterVolume/Lwater
+nx = round(Lwater / eleLen * numx)
+ny = round(Hwater / eleLen * numy)
+
+print(f'nx = {nx/numx}')
+print(f'ny = {ny/numy}')
+
+lower = [-L, -L]
+upper = [2 * L, 2 * H]
+
+eleArgs = ['PFEMElementBubble', rho, mu, b1, b2, thk, kappa]
+partArgs = ['quad', 0.5*L-Lwater/2.0, 0.0, 0.5*L+Lwater/2.0, 0.0,
+            0.5*L+Lwater/2.0, Hwater, 0.5*L-Lwater/2.0, Hwater, nx, ny]
+parttag = 4
+ops.mesh('part', parttag, *partArgs, *eleArgs)
+
+ops.mesh('bg', eleLen, *lower, *upper,
+         '-structure', sid, len(sNodes), *sNodes,
+         '-structure', sid, len(wallNodes1), *wallNodes1,
+         '-structure', sid, len(wallNodes2), *wallNodes2)
+
+# create constraint object
+ops.constraints('Plain')
+
+# create numberer object
+ops.numberer('Plain')
+
+# create convergence test object
+ops.test('PFEM', 1e-5, 1e-5, 1e-5, 1e-5, 1e-5, 1e-5, 100, 3, 1, 2)
+
+# create algorithm object
+ops.algorithm('Newton')
+
+# create integrator object
+ops.integrator('PFEM', 0.5, 0.25)
+
+# create SOE object
+# system('PFEM')
+ops.system('PFEM', '-mumps')
+
+# create analysis object
+ops.analysis('PFEM', dtmax, dtmin, b2)
+
+ops.analyze()
