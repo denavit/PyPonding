a5720ee02cf0469e83bc13c41d9ee1bbd92a36c2
denavit <denavit@gmail.com>
2022-05-31 22:00:26 -0400

    Create idealized_joist_bay.py

--- PyPonding/structures/__init__.py
+++ PyPonding/structures/__init__.py
@@ -3,3 +3,4 @@ from .steel_beam import steel_beam
 from .wide_flange import wf
 from .elastic_beam import ElasticBeam2d
 from .idealized_bay import IdealizedBay
+from .idealized_joist_bay import IdealizedJoistBay
