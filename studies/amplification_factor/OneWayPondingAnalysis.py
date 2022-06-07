34eb64b7f4e48a9118ed5e9cfc55f19589d323a8
denavit <denavit@gmail.com>
2022-06-07 09:49:01 -0400

    Update OneWayPondingAnalysis.py

--- studies/amplification_factor/OneWayPondingAnalysis.py
+++ studies/amplification_factor/OneWayPondingAnalysis.py
@@ -1,7 +1,7 @@
-from PyPonding.structures import steel_beam
-from PyPonding import FE
+from PyPonding.structures import ElasticBeam2d
 from math import pi, cos, cosh
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Define units
 inch    = 1.0
@@ -11,84 +11,72 @@ ft      = 12.0*inch
 ksi     = kip/inch**2
 psf     = lb/ft**2
 pcf     = psf/ft
+in_per_ft = inch/ft
 
 
 # Input parameters
-E       = 29000.0*ksi   # Modulus of elasticity
-I       = 75*inch**4   # Moment of inertia
-
 L       = 40*ft         # Beam span
-TW      = 5*ft          # Tributary width
-c       = 0*inch        # Camber
-
-qD      = 0.0*psf      # Dead load (force per area)
+S       = 5*ft          # Tributary width
+E       = 29000.0*ksi   # Modulus of elasticity
+I       = 75*inch**4    # Moment of inertia
 gamma   = 62.4*pcf      # Unit weight of water
-
-zi      = 0*inch        # Elevation of left end
-zj      = 0*inch        # Elevation of right end
+qD      = 0*psf         # Dead loda
+slope   = 0*in_per_ft # Beam slope
 zw      = 2*inch        # Elevation of water
 
 
-
-# Define Steel Beam Object
-beam = steel_beam();
-
-beam.L  = L
-beam.tw = TW
-beam.zi = zi
-beam.zj = zj
-beam.c  = c
-
-beam.E  = E
-beam.A  = 1000.0
-beam.I  = I
-
-beam.alpha  = 1.0
-beam.LF_D   = 1.0
-beam.wd     = qD
-beam.LF_P   = 1.0
-beam.gamma  = gamma
-beam.LF_S1  = 0.0
-beam.LF_S2  = 0.0
-beam.gammas = 0.0
-beam.hs     = 0.0
-
-beam.nele   = 40
-beam.BuildModel();
+# Build beam object
+beam = ElasticBeam2d(L,S,E,I,gamma,zj=slope*L,qD=qD)
+beam.num_elements = 60
 
 # Run Ponding Analysis
-PA = FE.PondingAnalysis(beam.model,'Constant_Level')
-PA.output_level = 1
-PA.max_iterations_z = 60
-res = PA.run({'DEAD':1.0},zw)
-if res != 0:
-    print('Not converged')
-Vmax = beam.Maximum_Shear(PA)
-Mmax = beam.Maximum_Moment(PA)
-(x,M,V) = beam.Moment_and_Shear(PA)
-x_at_Mmax = x[np.argmax(np.absolute(M))]
-
+resultsP = beam.run_analysis_OPS('IterativeLevel',target_zw=zw)
+print(resultsP.shear_along_length)
+VmaxP = np.amax(np.absolute(resultsP.shear_along_length))
+MmaxP = np.amax(resultsP.bending_moment_along_length)
+x_at_MmaxP = resultsP.position_along_length[np.argmax(resultsP.bending_moment_along_length)]
 
 # Run First-Order Analysis
-PAo = FE.PondingAnalysis(beam.model,'No_Ponding_Effect')
-res = PAo.run({'DEAD':1.0},zw)
-if res != 0:
-    print('Not converged')
-Vmaxo = beam.Maximum_Shear(PAo)    
-Mmaxo = beam.Maximum_Moment(PAo)
-(x,M,V) = beam.Moment_and_Shear(PAo)
-x_at_Mmaxo = x[np.argmax(np.absolute(M))]
+beam.include_ponding_effect = False
+results1 = beam.run_analysis_OPS('IterativeLevel',target_zw=zw)
+Vmax1 = np.amax(np.absolute(results1.shear_along_length))
+Mmax1 = np.amax(results1.bending_moment_along_length)
+x_at_Mmax1 = results1.position_along_length[np.argmax(results1.bending_moment_along_length)]
 
 
 # Print Results
-Cs = (gamma*TW*L**4)/(pi**4*E*I)
+Cs = (gamma*S*L**4)/(pi**4*E*I)
 print('\nCs = %.3f' % Cs)
 print('Basic Amplification 1/(1-Cs) = %.3f' % (1/(1-Cs)))
 x = pi/2*Cs**0.25
 print('More Exact Amplification     = %.3f\n' % ((1/cos(x)-1/cosh(x))/x**2)) # from Silver (2010)
 print('                      with ponding           no ponding          amplification')
-print('Max Shear           %9.3f kip         %9.3f kip          %9.3f' % (Vmax,Vmaxo,Vmax/Vmaxo))
-print('Max Moment          %9.3f kip-in      %9.3f kip-in       %9.3f' % (Mmax,Mmaxo,Mmax/Mmaxo))
-print('Pos. of Mmax      %5.1f in (%4.3f*L)    %5.1f in (%4.3f*L)\n' % (x_at_Mmax,x_at_Mmax/L,x_at_Mmaxo,x_at_Mmaxo/L))
+print('Max Shear           %9.3f kip         %9.3f kip          %9.3f' % (VmaxP,Vmax1,VmaxP/Vmax1))
+print('Max Moment          %9.3f kip-in      %9.3f kip-in       %9.3f' % (MmaxP,Mmax1,MmaxP/Mmax1))
+print('Pos. of Mmax      %5.1f in (%4.3f*L)    %5.1f in (%4.3f*L)\n' % (x_at_MmaxP,x_at_MmaxP/L,x_at_Mmax1,x_at_Mmax1/L))
 
 
+# Plot Results
+plt.figure()
+plt.plot(resultsP.position_along_length,resultsP.bending_moment_along_length,label='With Ponding')
+plt.plot(results1.position_along_length,results1.bending_moment_along_length,label='No Ponding')
+plt.legend()
+plt.ylabel('Bending Moment')
+plt.xlim([0,L])
+
+plt.figure()
+plt.plot(resultsP.position_along_length,resultsP.shear_along_length,label='With Ponding')
+plt.plot(results1.position_along_length,results1.shear_along_length,label='No Ponding')
+plt.legend()
+plt.ylabel('Shear')
+plt.xlim([0,L])
+
+plt.figure()
+plt.plot(resultsP.position_along_length,resultsP.bending_moment_along_length/results1.bending_moment_along_length,label='Bending Moment')
+plt.plot(results1.position_along_length,resultsP.shear_along_length/results1.shear_along_length,label='Shear')
+plt.legend()
+plt.ylabel('Amplification Factor')
+plt.xlim([0,L])
+#plt.ylim([0,2*(1/(1-Cs))])
+
+plt.show()
